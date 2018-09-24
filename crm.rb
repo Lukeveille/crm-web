1a09c80a980afdea421f8210d2fbd8e38715e335
Lukeveille <luke@lukeleveille.com>
2018-09-24 15:37:21 -0400

    Able to succesfully edit a contact, added consistent formatting to display and edit pages

--- crm.rb
+++ crm.rb
@@ -16,26 +16,51 @@ get "/contacts/new" do
   erb :new
 end
 
-get "/contacts/:id" do
-  params[:id]
-  @contact = Contact.find_by({id: params[:id].to_i})
-  if @contact
-    erb :show_contact
-  else
-    erb :error
-  end
-end
-
 post '/contacts' do
   Contact.create(
     first_name: params[:first_name],
     last_name:  params[:last_name],
     email:      params[:email],
     note:       params[:note]
-  )
-  redirect to('/contacts')
-end
+    )
+    redirect to('/contacts')
+  end
+  
+  get "/contacts/:id" do
+    params[:id]
+    @contact = Contact.find_by({id: params[:id].to_i})
+    if @contact
+      erb :show_contact
+    else
+      erb :error
+    end
+  end
 
+  get '/contacts/:id/edit' do
+    params[:id]
+    @contact = Contact.find_by(id: params[:id].to_i)
+    if @contact
+      erb :edit_contact
+    else
+      raise Sinatra::NotFound
+    end
+  end
+  
+  put '/contacts/:id' do
+    @contact = Contact.find_by(id: params[:id].to_i)
+    if @contact
+      @contact.update(
+      first_name: params[:first_name],
+      last_name:  params[:last_name],
+      email:      params[:email],
+      note:       params[:note]
+      )
+  
+      redirect to('/contacts')
+    else
+      raise Sinatra::NotFound
+    end
+  end
 
 get "/about" do
   @title = "About Me"
